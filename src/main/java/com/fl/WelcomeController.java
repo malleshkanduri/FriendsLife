e3b6e153e7493aa648b1f81f80a0b0d47da0f3a9
Mallesh Kanduri <mallesh.kanduri@gmail.com>
2018-04-12 13:18:43 -0500

    loginsuccess page

--- src/main/java/com/fl/WelcomeController.java
+++ src/main/java/com/fl/WelcomeController.java
@@ -2,20 +2,43 @@ package com.fl;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WelcomeController {
 
-	// inject via application.properties
-	@Value("${welcome.message:test}")
+	/*@Value("${welcome.message:test}")
 	private String message = "Hello World";
-
+*/
 	@RequestMapping("/testjsp")
 	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
+		//model.put("message", this.message);
 		return "welcome";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	
+	public String authenticateUser( @RequestParam("loginId") String loginId,
+									@RequestParam("password") String password,
+									Model model) {
+		System.out.println("loginId" + loginId + " password " + password);
+		
+		model.addAttribute("loginId_value", loginId);
+		
+		return "loginsuccess";
+	}
+	
+	public void validate () {
+		/*if ( request.getSession().getAttribute("user")==null) {  
+		    System.out.println("hello");
+		     return "redirect:/login.html"
+		    }
+		else 
+		   return "index";
+		}*/
 	}
 }
