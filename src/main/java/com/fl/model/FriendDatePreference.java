93312b6ee9bff0de4f89da342c57111bce5e48df
gkanudri <33337432+gkanudri@users.noreply.github.com>
2018-04-13 14:11:43 -0500

    new file

--- src/main/java/com/fl/model/FriendDatePreference.java
+++ src/main/java/com/fl/model/FriendDatePreference.java
@@ -1,5 +1,25 @@
 package com.fl.model;
 
+import java.util.List;
+
 public class FriendDatePreference {
 
+	String friendId;
+	List<Day> days;
+
+	public List<Day> getDays() {
+		return days;
+	}
+
+	public void setDays(List<Day> days) {
+		this.days = days;
+	}
+
+	public String getFriendId() {
+		return friendId;
+	}
+
+	public void setFriendId(String friendId) {
+		this.friendId = friendId;
+	}
 }
